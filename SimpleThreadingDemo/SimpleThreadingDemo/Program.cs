5366bfe627bbd8cf8ef2679bd32b84ebc24763ad
Ba Matrai <matrai.barnabas1@gmail.com>
2019-11-07 09:52:28 +0100

    create multiple threads

---
+++ SimpleThreadingDemo/SimpleThreadingDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleThreadingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadStart threadStart = new ThreadStart(Counting);
+            Thread thread = new Thread(threadStart);
+            Thread thread1 = new Thread(threadStart);
+            thread.Start();
+            thread1.Start();
+            thread.Join();
+            thread1.Join();
+            Console.ReadKey();
+        }
+
+        public static void Counting()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
