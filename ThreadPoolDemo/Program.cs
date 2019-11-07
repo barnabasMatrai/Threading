2bb895de159cafdc07a26e7a7e35e6fd75a15d68
Ba Matrai <matrai.barnabas1@gmail.com>
2019-11-07 12:00:00 +0100

    add ThreadPool project

---
+++ ThreadPoolDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPoolDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WaitCallback waitCallback = new WaitCallback(ShowMyText);
+            ThreadPool.QueueUserWorkItem(waitCallback, "hello");
+            ThreadPool.QueueUserWorkItem(waitCallback, "hi");
+            ThreadPool.QueueUserWorkItem(waitCallback, "Good day!");
+            Console.ReadKey();
+        }
+
+        public static void ShowMyText(object state)
+        {
+            string str = (string)state;
+            str += Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine(str);
+        }
+    }
+}
