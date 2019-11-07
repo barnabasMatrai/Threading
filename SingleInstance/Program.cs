8c585024cccb797b137a3ba9f17c1010d6526424
Ba Matrai <matrai.barnabas1@gmail.com>
2019-11-07 10:30:43 +0100

    add SingleInstance project with Mutex

---
+++ SingleInstance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SingleInstance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex mutex = null;
+            const string mutexName = "RUNMEONLYONCE";
+
+            while (true)
+            {
+                try
+                {
+                    mutex = Mutex.OpenExisting(mutexName);
+                    mutex.Close();
+                    Console.WriteLine("Closed existing Mutex");
+                }
+                catch (WaitHandleCannotBeOpenedException e)
+                {
+                    mutex = new Mutex(true, mutexName);
+                    Console.WriteLine("Created new Mutex");
+                }
+            }
+        }
+    }
+}
